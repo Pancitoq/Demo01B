a3c25b1292802af25d4b6ac6465a57c9b713b46a
Alumno <Alumno@LAB1507-12>
2024-08-20 19:51:15 -0500

    Agregar archivos de proyecto.

---
+++ Demo01B/Persona.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo01B
+{
+    public class Persona
+
+    {
+        public Persona()
+        {
+
+        }
+
+        public Persona (string nombres, string apellidos)
+        {
+            Nombres = nombres;
+            Apellidos = apellidos;
+        }
+
+        public String Nombres { get; set; }
+        public String Apellidos { get; set; }
+        public int Edad { get; set; }
+        public String NombresCompletos { get; set; }
+
+        public string DeolverEstado()
+        {
+            string mensaje = "Es menor de edad";
+            if (Edad > 18)
+                mensaje = "Es mayor de edad";
+            return mensaje;
+        }
+
+        public void AsignarNombresCompletos()
+        {
+            NombresCompletos = Nombres + " " + Apellidos;
+        }
+
+    }
+}
---
+++ Demo01B/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo01B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Triangulo triangulo = new Triangulo();
+            triangulo.Altura = 4;
+            triangulo.Base = 5;
+
+            Persona persona = new Persona();
+            persona.Nombres = "Ragde Luis";
+            persona.Apellidos = "Melendez Cora";
+
+            persona.AsignarNombresCompletos();
+            Console.WriteLine(persona.NombresCompletos);
+
+            Persona persona1 = new Persona("Jesus", "Cora");
+
+            Persona persona2 = new Persona
+            {
+                Nombres = "Derek",
+                Apellidos = "Torrico",
+                Edad = 5
+            };
+
+            Console.WriteLine(triangulo.CalcularArea())
+            Console.WriteLine("Hola mundo");
+            Console.Read();
+        }
+    }
+}
